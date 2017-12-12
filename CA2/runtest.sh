6fa4c2340781054eab3ecd5de3cabd533111feb2
sean Topham <sean.topham@ymail.com>
2017-12-12 13:54:00 +0000

    working script again, i think, cant check

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-echo -e "CO '\t' N '\t' Idle" > results.data
+test_duration=3
+
+echo -e "CO \t N \t Idle" > results.data
 
 for((i = 1; i <= $1; i++))
 do
 
 ./loadtest $1 &
-output = $(mpstat $test_duration 1 -o JSON | jq 'sysstat.host[0].statistics[0]."cpu-load"[0].idle')
-completions = $(cat synthetic.dat | wc -l)
+output = $( mpstat $test_duration 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle')
+pkill loadtest
+
+completions = $( cat synthetic.dat | grep -c "Transation Complete")
+
 
 echo -e "$completions \t $i \t $output" >> results.data
-pkill loadtest
+
 echo "Done $i"
 done
 fi
