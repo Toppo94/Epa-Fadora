068b080a28e010d66c6fc4ac84da510333b51703
sean Topham <sean.topham@ymail.com>
2017-12-12 13:23:52 +0000

    working script, i think, cant check

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+echo -e "CO '\t' N '\t' Idle" > results.data
 
-#for i {1..50}
-#do
-#./loadtest
-#timeout 1s 
-#done
+for((i = 1; i <= $1; i++))
+do
 
+./loadtest $1 &
+output = $(mpstat $test_duration 1 -o JSON | jq 'sysstat.host[0].statistics[0]."cpu-load"[0].idle')
+completions = $(cat synthetic.dat | wc -l)
 
-./loadtest 2
- mpstat -A 2 >> results.dat
-#mpstat -A >> results.dat
-echo "done" >> results.dat
-
+echo -e "$completions \t $i \t $output" >> results.data
+pkill loadtest
+echo "Done $i"
+done
+fi
