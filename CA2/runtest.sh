0ecd3005f5133691aefa8d5b7ec42ca1bbe7e807
Sean Topham <sean.topham@ymail.com>
2017-12-14 13:50:52 +0000

    Working Fully with Data

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-test_duration=3
+tduration=3
 
-echo -e "CO \t N \t Idle" > results.data
+echo -e "CO \t N \t Idle" > results.dat
 
 for((i = 1; i <= $1; i++))
 do
 
-./loadtest $1 &
-output = $( mpstat $test_duration 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle')
-pkill loadtest
-
-completions = $( cat synthetic.dat | grep -c "Transation Complete")
+	./loadtest $i &
+	output=$(mpstat $tduration 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle')
+	completions=$(cat synthetic.dat | wc -l)
 
 
-echo -e "$completions \t $i \t $output" >> results.data
+echo -e "$completions \t $i \t $output" >> results.dat
 
 echo "Done $i"
+
+pkill loadtest
+
 done
-fi
